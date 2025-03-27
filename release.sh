a708148eebefad5ea61022c48c1a2dad0ca2c0a1
Heiko Voigt <heiko.voigt@jimdo.com>
2025-03-27 16:03:36 +0100

    fix: set default remote for release

--- release.sh
+++ release.sh
@@ -16,4 +16,5 @@ major_tag=$(echo $NEW_TAG | sed -r 's/^(v[0-9]+).*$/\1/')
 git tag -f -a -m "Major Version $major_tag" $major_tag $NEW_TAG
 git push -f origin $major_tag
 
+gh repo set-default Jimdo/terraform-pr-commenter
 gh release create $NEW_TAG --generate-notes
